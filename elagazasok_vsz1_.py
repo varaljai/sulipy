7b163dc4379ac5b5278496f1f61487be51b3b1b5
varaljai <71698297+varaljai@users.noreply.github.com>
2021-08-10 13:32:13 +0200

    Add files via upload

---
+++ elagazasok_vsz1_.py
@@ -0,0 +1,6 @@
+a = input('Kérlek adj meg egy random számot 1-3 között. Én is ezt teszem és a végén ki írom mind kettönk eredményét.')
+import random
+
+szam = random.randint(1,3)
+
+print('Ez az én számom:',szam,'Ez pedig a tiéd:',a)
