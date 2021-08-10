8c56a12361769ee3b1708a70e2bdda04592509c3
varaljai <71698297+varaljai@users.noreply.github.com>
2021-08-10 13:26:32 +0200

    Add files via upload

---
+++ valtozok_ma1_.py
@@ -0,0 +1,6 @@
+a = int(input('Kérlek add meg a kőr sugarát és én megmodnom neked a terület-kerületét.'))
+
+PI = 3.14
+terulet = (a ** 2 * PI)
+kerulet = (a * 2 * PI)
+print('területe :' terulet ,"Kerülete :" kerulet, )
