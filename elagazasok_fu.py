f058a03f6b607be176ae8df8212d30b321a306e7
varaljai <71698297+varaljai@users.noreply.github.com>
2021-08-10 13:29:29 +0200

    Add files via upload

---
+++ elagazasok_fu.py
@@ -0,0 +1,7 @@
+a = input('Jó napod volt? ')
+if a == 'igen':
+    print('nagyszerű, Köszönöm a vászodat!')
+elif a == 'nem':
+    print('Sajnálom hogy nem, remélem holnap jobb lesz!! :3')
+else:
+    print('Ezt sajnos nem értem!')
